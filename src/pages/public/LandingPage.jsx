64246565770292bc9761aca81247e0ec0cb24edd
Nisha-20003 <aishatadebukola158@gmail.com>
2026-05-17 20:32:42 +0100

    last_phase

--- src/pages/public/LandingPage.jsx
+++ src/pages/public/LandingPage.jsx
@@ -9,6 +9,15 @@ import Footer from '../../components/layout/Footer'
 
 
 const LandingPage = () => {
+  let havePaid = false;
+  if(!havePaid){
+    return(
+      <div className='text-red-600 font-extrabold justify-center flex items-center h-[100vh] text-center flex-col px-7'>
+        <div className='mb-2 border-red-600 border-[1px] px-4 py-3 rounded-lg'>Error 404</div>
+      <div className=' text-[clamp(2rem,6vw,4.5rem)] '>Oops an Error has occurred. Come back later!!! </div>
+      </div>
+    )
+  }
 
   return (
     <div>
